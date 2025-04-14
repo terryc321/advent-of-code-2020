9d0d2e671d265c42c10867a60ccb693231048043
terry <terry.cadd@gmail.com>
2025-04-14 23:21:05 +0100

    day 14 aoc 2020
    haskell can now get strings grouped with mask and mems together in sequence
    required me to use chicken scheme to figure out the functional code
    still not convinced about the type checker though
    
    moving onto putting regular expressions into haskell code to extract two integers on
    each line for the mems a b
    also mask code ensure code is 36 characters long contains 0 1 or X only

--- day14/haskell/app/Main.hs
+++ day14/haskell/app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Typeable -- typeOf
 import System.IO
 
 -- True False True && False
@@ -10,13 +11,56 @@ isMask :: String -> Bool
 isMask s = length s == 43 && take 4 s == "mask"
 
 maskCode :: String -> String
-maskCode s = drop 5 s
+maskCode s = drop 7 s
 
-
+-- indentation is important in haskell
 
 -- total problem input stats
 -- 100 masks
 -- 464 mems 
+
+-- want mask to be associated with a series of mem structures in a list
+-- keep order as it MAY be important , append ++ if have to
+
+-- collect :: [String] -> [String] -> [[String]]
+-- collect [] [] acc = acc
+-- collect [] x acc = acc ++ [[x]]
+-- collect (h : t) x acc = if isMask h then collect t [h] (acc ++ [[x]])
+--                                     else collect t (x ++ [h]) acc
+--
+-- collect [] acc = acc
+-- collect (h : t) acc = if isMask h then collect t ([h] : acc)
+--                       else collect t ((head acc) )
+
+--- assume mask is well formed and comes first
+--- collect lines acc 
+collect [] acc = acc
+collect (h:t) acc = collect2 t h [] acc
+
+
+--collect2 lines mask mems acc
+collect2 [] mask mems acc = acc ++ [[(mask,mems)]]                            
+collect2 (h:t) mask mems acc =
+  if isMask h then collect (h:t) (acc ++ [[(mask,mems)]])
+  else let mem = h
+       in collect2 t mask (mems ++ [mem]) acc                     
+
+-- (define collect2 (lambda (lines mask mems acc)		   
+-- 		   (cond  ;; end of current mask - may have zero mems associated 
+-- 		    ((or (null? lines) (mask? (car lines)))
+-- 		     ;; (format #t "mask = ~a~%" mask)
+-- 		     ;; (pp mems)
+-- 		     ;; (format #t "~%")
+-- 		     (let ((acc2 (append acc (list (list mask mems)))))
+-- 		       (collect lines acc2)))
+-- 		    (#t ;; found a mem to attach to mask
+-- 		     (incf! count-mems)
+-- 		     (let ((mem (extract-mem (car lines))))
+-- 		       (collect2 (cdr lines) mask (append mems (list mem)) acc))))))
+-- (define (run-collect)
+--   (collect input '()))
+runCollect input = collect input []
+                                         
 
 -- print and show are closely related 
 -- ghci> :t print
@@ -33,23 +77,20 @@ showMasks ls = do
   -- mapM_ (\x -> putStrLn (show x)) masks
   -- mapM_ is monadic do something to each element and discard results
   mapM_ print masks
-  mapM_ print masks
-  
   -- mapM without underscore is monadic keep results
   fred <- mapM print masks
   putStrLn ("fred = " ++ (show fred))
   
   
-  
-
-  
 -- sometimes haskell will load with complaints but still load , just warnings look
 -- like it did not load
-main :: IO ()
+-- main :: IO ()
 main = do
   fileHandle <- openFile "../input.txt" ReadMode
   contents <- hGetContents fileHandle
-  let ls = lines contents 
+  let ls = lines contents
+  print (typeOf ls)
+  print (length contents)
   -- putStrLn contents
   -- putStrLn str
   -- show length of file
@@ -59,12 +100,16 @@ main = do
   -- show works sometimes not others , bracketting problem perhaps
   -- putStrLn (show [[(1::Int),2,3],[4,5,6]] )
   showMasks ls
-  -- mapM_ print ls
-  
+  putStrLn "lets see if we can do this with runCollect"
+  mapM_ (\x -> do print x
+                  putStrLn "") (runCollect ls)
+    
   -- putStrLn ("these are masks " ++ (show (map isMask ls)))
   -- p (\x -> putStrLn ("in>" ++ x)) ls
   hClose fileHandle
+  
 
+  
   
 
 
