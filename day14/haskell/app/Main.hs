6b042c9f45dd3e544854a9cfba69e1899e41f28a
terry <terry.cadd@gmail.com>
2025-04-14 21:33:37 +0100

    mask code
    advent of code 2020

--- day14/haskell/app/Main.hs
+++ day14/haskell/app/Main.hs
@@ -5,11 +5,41 @@ import System.IO
 -- True False True && False
 -- poor mans parser
 isMask :: String -> Bool
-isMask s = length s > 4 && take 4 s == "Mask"
---isMask s = length s == 43 && take 4 s == "Mask"
+-- moved mysteriously from uppercase Mask to lowercase mask
+--isMask s = length s > 4 && take 4 s == "mask"
+isMask s = length s == 43 && take 4 s == "mask"
 
-showMasks :: IO ()
-showMasks = do
+maskCode :: String -> String
+maskCode s = drop 5 s
+
+
+
+-- total problem input stats
+-- 100 masks
+-- 464 mems 
+
+-- print and show are closely related 
+-- ghci> :t print
+-- print :: Show a => a -> IO ()
+-- ghci> :t show
+-- show :: Show a => a -> String
+showMasks :: [String]-> IO ()
+showMasks ls = do
+  putStrLn "showmasks doing something"
+  -- map (\x -> if isMask x then putStrLn (show x) else putStr "" ) ls
+  let masks = filter isMask ls
+  let masksCount = length masks
+  putStrLn ("There are " ++ (show masksCount) ++ " masks found.")
+  -- mapM_ (\x -> putStrLn (show x)) masks
+  -- mapM_ is monadic do something to each element and discard results
+  mapM_ print masks
+  mapM_ print masks
+  
+  -- mapM without underscore is monadic keep results
+  fred <- mapM print masks
+  putStrLn ("fred = " ++ (show fred))
+  
+  
   
 
   
@@ -29,6 +59,8 @@ main = do
   -- show works sometimes not others , bracketting problem perhaps
   -- putStrLn (show [[(1::Int),2,3],[4,5,6]] )
   showMasks ls
+  -- mapM_ print ls
+  
   -- putStrLn ("these are masks " ++ (show (map isMask ls)))
   -- p (\x -> putStrLn ("in>" ++ x)) ls
   hClose fileHandle
