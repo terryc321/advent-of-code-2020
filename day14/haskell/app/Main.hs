e74d27e3836a23dea9c56e9a43a45b3dca51a255
terry <terry.cadd@gmail.com>
2025-04-15 01:47:15 +0100

    solved part 1 of day 14 aoc2020
    worked out really well
    
    mask and mems dest <- pre-value
    [("00000X110010111111X000100XX01010000X",[[20690,435],[54036,231],[27099,118644255],[55683,22299263],[26119,2279399]])]
    
    split to
      mask dest pre-value  tuples
    [("00000X110010111111X000100XX01010000X",20690,435),("00000X110010111111X000100XX01010000X",54036,231),("00000X110010111111X000100XX01010000X",27099,118644255),("00000X110010111111X000100XX01010000X",55683,22299263),("00000X110010111111X000100XX01010000X",26119,2279399)]
    
    folded all mask-mems all together
    into a long sequence of
      mask dest pre-value  tuples
    ("00000X110010111111X000100XX01010000X",20690,435)
    ("00000X110010111111X000100XX01010000X",54036,231)
    ("00000X110010111111X000100XX01010000X",27099,118644255)
    
    computed post-values using mask and pre-value
    to create a tuple list (dest , masked-value-to-be-written )
    
    if 20690 occurs later in the list then this entry can be disgarded as its going to be overwritten
    otherwise we simply sum the 2nd elements to a running total
    
    (20690,855384225)
    (54036,855384225)
    (27099,855516833)
    (55683,855384737)
    (26119,855516833)
    (42072,50265285)
    
    when list is empty simply report the run total
    
    (2666,13018599784)
    (21482,13001823192)
    --- end of g ---
    --- tot computed ---
    --- tot = 4297467072083   ....... ANSWER accepted !
    --- tot end ---

--- day14/haskell/app/Main.hs
+++ day14/haskell/app/Main.hs
@@ -40,7 +40,7 @@ foo2 = getAllTextMatches (("john anne yifan" :: String) =~ ("[a-z]+" :: String))
 
 -- ok so finally finished getting some integers from a string - at last
 extractInts str =  let re = "[0-9]+" 
-                   in map (\x -> read x :: Int)
+                   in map (\x -> read x :: Integer)
                       (getAllTextMatches ((str :: String) =~ (re::String))::[String])
 
 -- avoid maybe's like plague
@@ -181,6 +181,23 @@ m3 = maskValue (compute "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X" 0)
 -- beforeVal = 11 
 -- dest 8
 
+-- runOrder takes this data
+-- [("00000X110010111111X000100XX01010000X",[[20690,435],[54036,231],[27099,118644255],[55683,22299263],[26119,2279399]])]
+-- and makes this 
+-- [("00000X110010111111X000100XX01010000X",[20690,435]
+-- [("00000X110010111111X000100XX01010000X",[54036,231]
+-- [("00000X110010111111X000100XX01010000X",[27099,118644255]
+-- [("00000X110010111111X000100XX01010000X",[55683,22299263]
+-- [("00000X110010111111X000100XX01010000X",[26119,2279399]
+
+-- acc is where we put the result
+-- runOrder3 mask mems acc = 
+
+runOrder3 mask [] acc = acc
+runOrder3 mask ([a,b]:mt) acc = runOrder3 mask mt (acc ++ [(mask,a,b)])
+runOrder2 (mask,mems) = runOrder3 mask mems []
+runOrder xs = runOrder2 (head xs)
+
 
 -- print and show are closely related 
 -- ghci> :t print
@@ -200,7 +217,34 @@ showMasks ls = do
   -- mapM without underscore is monadic keep results
   fred <- mapM print masks
   putStrLn ("fred = " ++ (show fred))
-  
+
+-- memory space is written to
+-- format (destination , value )
+-- if destination occurs later in the list , then it will get a new value then
+-- so no point in doing anything with it now because its value in memory space is
+-- overwritten later on
+--
+-- otherwise nobody write to this memory location later , so this is the value it
+-- has at end of the run
+-- so sum it up
+--
+-- if no more values then the result is the sum s
+-- we are done
+memSpace :: [(Integer,Integer)] -> Integer -> Integer  
+memSpace [] s = s
+memSpace ((d,v):t) s = if memIn d t then memSpace t s
+                       else memSpace t (s+v)
+
+-- does destination d occur later on in the memory writes                            
+memIn d [] = False
+memIn d ((d2,v2):t) = if d == d2 then True
+                      else memIn d t
+                          
+
+
+
+
+
   
 -- sometimes haskell will load with complaints but still load , just warnings look
 -- like it did not load
@@ -222,13 +266,50 @@ main = do
   showMasks ls
   putStrLn "lets see if we can do this with runCollect"
   let rc = runCollect ls
+  let pc = map runOrder rc
+  
   mapM_ (\x -> do print x
                   putStrLn "") rc 
+
+  putStrLn "-- input to runOrder ---"
+  print (head rc)
+  putStrLn "-- lets see if we can do this with runOrder ---"
+  mapM_ (\x -> do print x
+                  putStrLn "") pc 
+  putStrLn "--- end of runOrder ---"
+
+  -- f is a list of mask dest val 
+  -- dest : where value eventually written
+  -- val  : value before converted to 36 bit and masked
+  -- mask : string 36 chars long represent the 36 bit mask
+  let f = foldr (++) [] pc
+  mapM_ (\x -> do print x ) f 
+
+  putStrLn "--- end of f ---"
+
+  -- g 
+  let g = map (\(mask,dest,val) -> (dest,maskValue (compute mask val))) f
+  mapM_ (\x -> do print x ) g 
+  
+  putStrLn "--- end of g ---"
+
+  putStrLn "--- tot computed ---"
+  let tot = memSpace g 0
+  putStrLn ("--- tot = " ++ (show tot))
+  putStrLn "--- tot end ---"
+-- --- tot computed ---
+-- --- tot = 4297467072083
+-- --- tot end ---
+-- ghci> binarz 4297467072083
+-- [1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,0,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,1,0,1,0,0,1,1]
+-- ghci> length (binarz 4297467072083)
+-- 42
     
   -- putStrLn ("these are masks " ++ (show (map isMask ls)))
   -- p (\x -> putStrLn ("in>" ++ x)) ls
   hClose fileHandle
   
+
 
   
   
