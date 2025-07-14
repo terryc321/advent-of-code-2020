c714e72c396858549b38ff9c8043be3907ca9b09
terry <terry.cadd@gmail.com>
2025-07-14 14:58:33 +0100

    parsing file

--- day19/chicken/fun.scm
+++ day19/chicken/fun.scm
@@ -20,6 +20,8 @@ have to parse this line
 
 |#
 
+(import (chicken pretty-print))
+
 ;; for current directory
 (import (chicken process-context))
 ;;(current-directory)
@@ -29,18 +31,42 @@ have to parse this line
 ;; for readlines 
 (import (chicken io))
 
+;; word count??
+(define input-lines 
+  (with-input-from-file "../input.txt" (lambda ()
+					 (read-lines))))
 
+;; split each line based on space character
+(define split-input-lines (map (lambda (s)
+				 (cond
+				  ((= 0 (string-length s)) '())
+				  ((or (char=? (string-ref s 0) #\a)
+				       (char=? (string-ref s 0) #\b))
+				   s)
+				  (#t (string-split-fields "[^ ]+" s))))
+			       input-lines))
+
+
+
+
+
+
+
+
+
+#|
+;; word count a file 
 (with-input-from-file "../input.txt" (lambda ()
 				       (let loop ((count 0) (c (read-char)))
 					 (if (eof-object? c) count
 					     (loop
 					      (if (char=? c #\newline) (+ count 1) count)
-					      (read-char))))))
+                                              (read-char))))))
+|#
 
 
 ;; using regex module
 (import regex)
 
-(let ((s "66: 69 116 | 9 115")) (string-split-fields "[^ ]+" s))
 
 (define (foo) 22)
