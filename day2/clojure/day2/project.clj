57b7e8cc867029505535d588b35d6ae2cee2c7ad
terry <terry.cadd@gmail.com>
2025-10-08 10:41:49 +0100

    completed day2 in clojure using antlr parser

--- day2/clojure/day2/project.clj
+++ day2/clojure/day2/project.clj
@@ -3,7 +3,8 @@
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.2"]]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [clj-antlr "0.2.14"]]
   :main ^:skip-aot day2.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- day2/clojure/day2/src/day2/core.clj
+++ day2/clojure/day2/src/day2/core.clj
@@ -1,7 +1,188 @@
 (ns day2.core
   (:gen-class))
 
+(require ['clj-antlr.core :as 'antlr])
+
+(def json (antlr/parser "grammars/Json.g4"))
+
+(def parser (antlr/parser "grammars/Day2.g4"))
+
+;; slurp file ?
+(def input (slurp "../../input-pristine"))
+(def string-lines (clojure.string/split-lines input))
+
+
+;; ;; drop 1st and last char from input for some reason we have parens around the input text
+;; (defn butfirst-last
+;;   [str]
+;;   (let* [word str
+;;          wordlen (count word)]
+;;     (if (< wordlen 2)
+;;       ""
+;;       (let* [word1 (subs word 0 (- wordlen 1))
+;;              word2 (subs word1 1)]
+;;         word2))))
+;; (butfirst-last "alpha")
+;; (butfirst-last "")
+
+;;(def string-lines (map butfirst-last string-lines))
+
+;;(def test-string1 (str (get string-lines 0) \newline))
+(def test-string1 (get string-lines 0))
+
+
+;; add newline to string
+;; (conj \n "123")
+;; (str "123" \newline )
+
+
+(count "alpha")
+
+(subs "alpha" 1)
+
+(pprint (json "[1,2,3]"))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(pprint (parser test-string1))
+
+(nth (parser test-string1) 0)
+(nth (parser test-string1) 1) ;; 
+(nth (parser test-string1) 2)
+(nth (parser test-string1) 3) ;; 
+(nth (parser test-string1) 4)
+(nth (parser test-string1) 5) ;; 
+(nth (parser test-string1) 6)
+(nth (parser test-string1) 7)
+(nth (parser test-string1) 8) ;;
+
+(def ast (map (fn [str]
+                (let [x (parser str)]
+                  (list (Integer. (nth x 1))
+                        (Integer. (nth x 3))
+                        (nth (nth x 5) 0)
+                        (nth x 8))))
+              string-lines))
+
+;; (def ast2 (map (fn [x] x) string-lines))
+
+;; does each character occur that many times in a string ?
+(defn occurs
+  "how many times a given character appears in a string"
+  [ch str]
+  (let [len (count str)]
+    (defn f [i c]
+      (loop [i i
+             c c]
+        (cond
+          (>= i len) c
+          (= (nth str i) ch) (recur (+ i 1) (+ c 1))
+          true (recur (+ i 1) c))))
+    (f 0 0)))
+
+(occurs \a "aaa")
+(occurs \b "acbac")
+(occurs \c "abd")
+(occurs \a "")
+
+
+;; for each ast , find how many times character appears in string
+;; does character appear required number of times 
+
+(defn ast-f  
+  [x]
+  (let* [from (nth x 0)
+         to (nth x 1)
+         ch (nth x 2)
+         word (nth x 3)
+         occ (occurs ch word)
+         ]
+    (and (>= occ from) (<= occ to))))
+    
+
+(ast-f (nth ast 0))
+
+
+(map ast-f ast)
+
+(filter (fn [x] (if x x false)) (map ast-f ast))
+
+(def part1 (count (filter (fn [x] (if x x false)) (map ast-f ast))))
+;; part1 => 542
+;; accepted answer
+
+
+;; part2 the two numbers in the ast say letter must occur only either at 1st number or 2nd number , but not both
+;; to be a valid password
+
+(defn dodgy-nth
+  "if index n supplied into string str then return false otherwise compare with character ch"
+  [str n ch]
+  (let [len (count str)]
+    (cond
+      (>= n len) false
+      (< n 0) false
+      true (= (nth str n) ch))))
+
+(dodgy-nth "word" 0 \w)
+(dodgy-nth "word" 1 \o)
+(dodgy-nth "word" 2 \r)
+(dodgy-nth "word" 3 \d)
+(dodgy-nth "word" 4 \?)
+    
+
+;; off by 1 error - nth 0 is first character , whereas 1 is first character in puzzle
+;; 
+
+(defn ast-valid?
+  [x]
+  (let* [from (nth x 0)
+         to (nth x 1)
+         ch (nth x 2)
+         word (nth x 3)
+         occ1 (dodgy-nth word from ch)
+         occ2 (dodgy-nth word to ch)
+         ]
+    (cond
+      (and occ1 (not occ2)) true
+      (and occ2 (not occ1)) true
+      true false)))
+
+;; filter truthy values , then count how many we got
+(def part2 (count (filter (fn [x] (if x x false)) (map ast-valid? ast))))
+;; part2 ==> 434
+;; wrong ! what went wrong ??
+;;
+
+
+
+(defn ast-valid?
+  [x]
+  (let* [from (nth x 0)
+         to (nth x 1)
+         ch (nth x 2)
+         word (nth x 3)
+         occ1 (dodgy-nth word (- from 1) ch)
+         occ2 (dodgy-nth word (- to 1) ch)
+         ]
+    (cond
+      (and occ1 (not occ2)) true
+      (and occ2 (not occ1)) true
+      true false)))
+
+;; filter truthy values , then count how many we got
+(def part2 (count (filter (fn [x] (if x x false)) (map ast-valid? ast))))
+;; part2 ==> 360
+;; accepted answer
+
+;; mistake or confusion comes from string indexing
+;; in puzzle itself uses 1 as 1st index into string
+;; clojure and many other languages use 0 as 1st index into string
+
+
+
+
+
